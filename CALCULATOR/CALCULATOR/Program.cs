813354d219b9d7092309ed970137ef9b9a23f9b7
Pamu1234 <sparmeshwar@qualminds.com>
2022-05-23 18:06:01 +0530

    Basic Calculator Program added

---
+++ CALCULATOR/CALCULATOR/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using functions;
+Console.WriteLine("Enter first number :");
+int num1=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter operator :");
+string opr = Console.ReadLine();
+
+Console.WriteLine("Enter second number :");
+int num2=Convert.ToInt32(Console.ReadLine());
+
+switch(opr)
+{
+    case "+":
+        var result = Class1.Addition(num1, num2);
+        Console.WriteLine(result);
+        break;
+
+    case "-":
+         result = Class1.Subtraction(num1, num2);
+        Console.WriteLine(result);
+        break;
+
+    case "*":
+        result = Class1.Multiplication(num1, num2);
+        Console.WriteLine(result);
+        break;
+
+    case "/":
+        result = Class1.Division(num1, num2);
+        Console.WriteLine(result);
+        break;
+
+    case "%":
+        result = Class1.Modulus(num1, num2);
+        Console.WriteLine(result);
+        break;
+
+        default:
+        Console.WriteLine("invalid input");
+        break;
+}
---
+++ CALCULATOR/functions/Class1.cs
@@ -0,0 +1,26 @@
+﻿namespace functions
+{
+    public class Class1
+    {
+        public static int Addition(int num1,int num2)
+        { 
+            return num1 + num2; 
+        }
+        public static int Subtraction(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public static int Multiplication(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public static int Division(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+        public static int Modulus(int num1, int num2)
+        {
+            return num1 % num2;
+        }
+    }
+}
