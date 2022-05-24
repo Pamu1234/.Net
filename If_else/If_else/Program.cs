178f664c3e904d2c6e992d5cb7e19e27f9f7e4b6
Pamu1234 <sparmeshwar@qualminds.com>
2022-05-24 18:23:36 +0530

    adding two new projects

---
+++ If_else/If_else/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+int x = 10;
+int y = 5;
+
+if(x>y)
+{
+    Console.WriteLine("Value of x is large");
+}
+else
+{
+    Console.WriteLine("Value of y is large");
+}
+
+
+
+
+
+
+// IF AND ELSE IF  
+int a = 10;
+int b = 5;
+
+if (a == b)
+{
+    Console.WriteLine("A & B ARE EQUAL");
+}
+else if (a>b)
+{
+    Console.WriteLine("A GREATER THAN B");
+}
+else
+{
+    Console.WriteLine("Change the values");
+}
+
+     
+
---
+++ Looping/Looping/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Creating an aaray and acces it's element by using for loop.
+
+string[] bike = { "Jawa", "Bullet", "Splendor", "Ktm" };
+
+for (int i = 0; i < bike.Length ; i++)
+{
+    Console.WriteLine(bike[i]);
+
+}
+
+
+// basic program to check even and odd numbers
+for (int i = 0; i < 100; i++)
+{
+    if (i%2==0)
+    {
+        Console.WriteLine(i + " is a even number ");
+    }
+    else
+    {
+        Console.WriteLine(i + " is odd number");
+    }
+}
