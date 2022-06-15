d7551f1e127034cfcd1f4099aef68c02636f56bc
Pamu1234 <sparmeshwar@qualminds.com>
2022-06-16 00:12:10 +0530

    Basic EMI calculator project added.

---
+++ BasicEmiCalculator/BasicEmiCalculator/Program.cs
@@ -0,0 +1,11 @@
+﻿using EmiCalculations;
+
+Console.WriteLine("Enter product price:");
+long totalAmount = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter down payment:");
+long emiAmount = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Emi.EmiCalculatorForTwelveMonths(totalAmount, emiAmount));
+Console.WriteLine(Emi.EmiCalculatorForSixMonths(totalAmount, emiAmount));
+Console.WriteLine(Emi.EmiCalculatorForThreeMonths(totalAmount, emiAmount));
---
+++ BasicEmiCalculator/EmiCalculations/Emi.cs
@@ -0,0 +1,30 @@
+﻿namespace EmiCalculations
+{
+    public class Emi
+    {
+        public static string EmiCalculatorForTwelveMonths(long totalAmount, long Downpayment)
+        {
+            long loanAmount = totalAmount - Downpayment;
+            long emiAmount = loanAmount/12;
+            
+            return $"Total EMI amount is {emiAmount}, for yearly";
+        }
+
+        public static string EmiCalculatorForSixMonths(long totalAmount, long Downpayment)
+        {
+            long loanAmount = totalAmount - Downpayment;
+            long emiAmount = loanAmount / 6;
+
+            return $"Total EMI amount is {emiAmount}, for 6 months.";
+        }
+
+        public static string EmiCalculatorForThreeMonths(long totalAmount, long Downpayment)
+        {
+            long loanAmount = totalAmount - Downpayment;
+            long emiAmount = loanAmount / 3;
+
+            return $"Total EMI amount is {emiAmount}, for 3 months.";
+        }
+
+    }
+}
