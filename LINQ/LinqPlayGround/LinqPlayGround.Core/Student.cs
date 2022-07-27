07cb540d6ff40c01d6aef83cbe2accf7f52881a8
Pamu1234 <sparmeshwar@qualminds.com>
2022-07-27 17:04:28 +0530

    Created new queries.

---
+++ LINQ/LinqPlayGround/LinqPlayGround.Core/Student.cs
@@ -0,0 +1,29 @@
+﻿namespace LinqPlayGround.Core
+{
+    public class Student
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public int Id { get; set; }
+        public Dictionary<string, int> ?Scores;
+        public static List<Student> GetStudents()
+        {
+            return new List<Student>
+            {
+                new Student {FirstName="Svetlana", LastName="Omelchenko", Id=111, Scores = new Dictionary<string,int> { { "maths", 97 },{"science",92 },{"biology",81 },{ "social", 60 } } },
+                new Student {FirstName="Claire", LastName="O'Donnell", Id=112, Scores= new Dictionary<string,int> { { "maths", 75},{"science", 84 },{"biology", 91},{ "social", 39} } },
+                new Student {FirstName="Sven", LastName="Mortensen", Id=113, Scores= new Dictionary<string,int>   { { "maths", 88},{"science", 94}, {"biology", 65},{ "social", 91} } },
+                new Student {FirstName="Cesar", LastName="Garcia", Id=114, Scores= new Dictionary<string,int>     { { "maths", 97},{"science", 89}, {"biology", 85},{ "social", 82} } },
+                new Student {FirstName="Debra", LastName="Garcia", Id=115, Scores= new Dictionary<string,int>     { { "maths", 35},{"science", 72}, {"biology", 91},{ "social", 70} } },
+                new Student {FirstName="Fadi", LastName="Fakhouri", Id=116, Scores= new Dictionary<string,int>    { { "maths", 99},{"science", 86}, {"biology", 90},{ "social", 94} } },
+                new Student {FirstName="Hanying", LastName="Feng", Id=117, Scores= new Dictionary<string,int>     { { "maths", 93},{"science", 92}, {"biology", 80},{ "social", 87} } },
+                new Student {FirstName="Hugo", LastName="Garcia", Id=118, Scores= new Dictionary<string,int>      { { "maths", 92},{"science", 90}, {"biology", 83},{ "social", 78} } },
+                new Student {FirstName="Lance", LastName="Tucker", Id=119, Scores= new Dictionary<string,int>     { { "maths", 68},{"science", 79}, {"biology", 88},{ "social", 92} } },
+                new Student {FirstName="Terry", LastName="Adams", Id=120, Scores= new Dictionary<string,int>      { { "maths", 99},{"science", 81}, {"biology", 81},{ "social", 79} } },
+                new Student {FirstName="Eugene", LastName="Zabokritski", Id=121, Scores= new Dictionary<string,int>{ {"maths", 96},{"science", 85}, {"biology", 91},{ "social", 60} } },
+                new Student {FirstName="Michael", LastName="Tucker", Id=122, Scores= new Dictionary<string,int>    { { "maths",94},{"science", 91}, {"biology", 91},{ "social", 91} } },
+                new Student {FirstName="Michael", LastName="Fastbender", Id=122, Scores= new Dictionary<string,int>{ { "maths",94},{"science", 92}, {"biology", 91},{ "social", 91} } }
+            };
+        }
+    }
+}
---
+++ LINQ/LinqPlayGround/LinqPlayGround/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LinqPlayGround.Core;
+
+
+
+
+List<Student> students = Student.GetStudents();
+
+var nameOfStudent = from name in  students orderby name.FirstName select new { name.FirstName, Name = name.FirstName + " " + name.LastName};
+Console.WriteLine("List of the student's:\n");
+foreach (var item in nameOfStudent)
+{
+    Console.WriteLine(item.Name);
+}
+Console.WriteLine($"Total number of students is = {nameOfStudent.Count()}");
+
+
+var topperformer = from name in students orderby name.Scores?.Values.Average() descending select  name;
+var top = topperformer.FirstOrDefault();
+
+Console.WriteLine($"\nTopper name is: {top?.FirstName} And percentage is: {top?.Scores?. Values.Average()}");
+
+Console.WriteLine( "\n----Math Topper---");
+
+var mathTopper = from name in students 
+                 orderby name.Scores["maths"] descending
+                 select new {Name = name.FirstName+" " + name.LastName, MathMarks = name.Scores["maths"] };
+
+var mathsTopper = 0;
+foreach (var marks in mathTopper)
+{
+    if (marks.MathMarks>= mathsTopper)
+    {
+        mathsTopper = marks.MathMarks;
+    }
+    else
+    {
+        break;
+    }
+    Console.WriteLine($"{marks.Name} {marks.MathMarks}");
+}
+
+Console.WriteLine("\n----Biology Topper---");
+
+var bioTopper = from name in students
+                orderby name.Scores["biology"] descending
+                select new { Name = name.FirstName + " " + name.LastName, BioMarks = name.Scores["biology"] };
+
+var biosTopper = 0;
+foreach (var marks in bioTopper)
+{
+    if (marks.BioMarks >= biosTopper)
+    {
+        biosTopper = marks.BioMarks;
+    }
+    else
+    {
+        break;
+    }
+    Console.WriteLine($"{marks.Name} {marks.BioMarks}");
+}
+
+
+//Console.WriteLine("\n---Highest percentage in Math & Bio---");
+//var mathAndBioTopper = from name  in students where name?.Scores?["maths"]>= 95 && name?.Scores?["biology"] >= 90 orderby name.Scores?["maths"],"biology"  select name;
+//foreach (var item in mathAndBioTopper)
+//{
+//    Console.WriteLine($"{item.FirstName} Math percentage is {item.Scores["maths"]} And Biology percentage is {item.Scores["biology"]}");
+//}
+
+//var topperformer = from name in students order by Average select name.Scores
+//foreach (var students in firstNameOfStudent)
+//{
+
+//}
+//var topPerfomer = from name in students
+
+// ---------------------------
+
+
+// public static List<Student> students = Student.GetStudents();
+//static void Main(string[] args)
+//{
+
+//    var studentsQuery = GetStudentsWithQuerySyntax(30, 1);
+//    //var studentsQuery = GetStudentsWithMethodSyntax(3, 2);
+
+//    // Projection transformation to string
+//    //var qualifiedStudents = from student in studentsQuery
+//    //                        select student.First + " " + student.Last;
+
+//    // Projection transformation to annonymous collection.
+//    var qualifiedStudents = from student in studentsQuery
+//                            select new { student.First, Name = student.First + " " + student.Last, Average = student.Scores.Average() };
+
+//    var qualifiedStudent = qualifiedStudents.FirstOrDefault();
+
+//    Console.Write(qualifiedStudent?.Name + " " + qualifiedStudent?.Average + "\n");
+
+
+//    //// First OR Default
+//    //var firstStudent = students.FirstOrDefault();
+//    //Console.WriteLine($"{firstStudent?.First} {firstStudent?.Last}");
+
+
+//    foreach (var student in studentsQuery)
+//    {
+//        Console.WriteLine($"{student.First} {student.Last} {student.Scores.Average()}");
+//    }
+//    Console.WriteLine(studentsQuery.Count());
+//}
+
+//public static IEnumerable<Student> GetStudentsWithQuerySyntax(int rowCount, int pageIndex)
+//{
+//    var skipRows = (pageIndex - 1) * rowCount;
+//    return (from student in students
+//            let average = student.Scores.Average()
+//            orderby average descending
+//            select student).Skip(skipRows).Take(rowCount);
+//}
+
+//public static IEnumerable<Student> GetStudentsWithMethodSyntax(int rowCount, int pageIndex)
+//{
+//    var skipRows = (pageIndex - 1) * rowCount;
+//    return students.OrderBy(st => st.First).OrderBy(st => st.Last).MySkip(skipRows).MyTake(rowCount);
+//}
+//    }
+//    static class StudentExtensions
+//{
+//    public static IEnumerable<Student> MySkip(this IEnumerable<Student> students, int value)
+//    {
+//        return students.Skip(value);
+//    }
+//    public static IEnumerable<Student> MyTake(this IEnumerable<Student> students, int value)
+//    {
+//        return students.Take(value);
+//    }
+//}
+//}
